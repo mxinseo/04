e95466202043d2228fc5392fdd73518fdc2fa0a2
rudalstj <jade201913@gmail.com>
2022-09-27 17:13:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int year;
-
-	printf("input year: ");
-	scanf("%d", &year);
-
-	if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-		printf("%d is leap year!\n", year);
-	else
-		printf("%d is not leap year\n", year);
-
+	unsigned int x;
+	int b;
+	
+	printf("input a number : ");
+	scanf("%ui", &x);
+	
+	for (b=0; x != 0; x >>= 1)
+	{
+		if(x & 1)
+		{
+			b = b + 1;
+		}
+	}
+	
+	printf("The result is : %i\n", b);
+	
 	return 0;
 }
